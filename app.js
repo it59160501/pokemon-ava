5811cb9e1877bf0baf93f963c95953f65793d577
Pimped Toloy <59160501@go.buu.ac.th>
2019-08-17 09:55:46 +0700

    Initalize Pokemon project V.2

--- app.js
+++ app.js
@@ -4,19 +4,39 @@ const port = 3000;
 
 app.use(express.json())
 
-let pokemons = [
-    {name:'Pikachu',type:'Electhic'},
-    {name:'Goduk',type:'Water'},
-    {name:'Charizard',type:'Fire'}
-]
+// {name:'Pikachu',type:'Electhic'},
+// {name:'Goduk',type:'Water'},
+// {name:'Charizard',type:'Fire'}
 
+class Pokemon{
+    constructor(name,type){
+        this.id=null
+        this.name=name
+        this.type=type
+    }
+}
+
+let pokemons = []
+pokemons.push(createPokemon('Pikachu','Electhic'))
+pokemons.push(createPokemon('Goduk','Water'))
+
+function createId(num){
+    return num + 1
+}
+
+function createPokemon(name,type){
+    let pokemon = new Pokemon(name,type)
+    pokemon.id = createId(pokemons.length)
+    return pokemon
+}
 
 app.get("/pokemons", (req, res) => res.send(pokemons))
 
 
 app.post("/pokemons", (req, res) => {
-    console.log(req.body)
-    pokemons.push(req.body)
+    let pokemon = createPokemon(req.body.name,req.body.type)
+    console.log(pokemon)
+    pokemons.push(pokemon)
     res.sendStatus(201)
 })
 
